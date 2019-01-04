e303c45158c067d3460ccfc5a87575e292ffe92c
Michael Andrew Chadwick <michael.andrew.chadwick@gmail.com>
2019-01-04 10:47:26 +0000

    update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Michael Andrew Chadwick | Software Developer`,
+    description: `Michael Andrew Chadwick, London based Software Developer.`,
+    author: `@nopenotmikey`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
   ],
 }
