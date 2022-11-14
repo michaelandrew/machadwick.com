8d5fa589bd903e04122a78123a93c6c01fcfbdc0
Michael Andrew Chadwick <michael.andrew.chadwick@gmail.com>
2022-11-14 22:03:22 +0000

    Default Page Layout

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,25 @@
+import { Karla } from '@next/font/google'
+
 type HeaderProps = {
   acronym: string,
 };
 
+const karla = Karla()
+
 const Header = ({ acronym }: HeaderProps) => (
-  <header className="header">
-    {acronym}
-  </header>
+  <>
+    <style>{`
+      .header {
+        margin: 24px;
+        font-size: 28px;
+        font-weight: 500;
+        font-family: ${karla.style.fontFamily}, Helvetica, Arial, sans-serif;
+      }
+    `}</style>
+    <header className="header">
+      {acronym}
+    </header>
+  </>
 )
 
 export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-const Home = () => {
-  return (
-    <></>
-  )
-}
+import Title from '../components/title'
+
+import config from '../config'
+
+const Home = () => (
+  <>
+    <Title 
+      title={config.siteMetadata.title} 
+      description={config.siteMetadata.description}
+    ></Title>
+  </>
+)
 
 export default Home
